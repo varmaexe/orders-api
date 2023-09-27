d393d30cc1f362944e2b36dc4256ac9666d42c53
sai verma <vectorvarma0303@gmail.com>
2023-09-27 15:08:20 +0530

    Data model for order added

--- model/order.go
+++ model/order.go
@@ -7,16 +7,16 @@ import (
 )
 
 type Order struct {
-	OrderID     uint64     `json:"order_id`
-	CustomerID  uuid.UUID  `json:"customer_id`
-	LineItems   []LineItem `json:"line_items`
-	CreatedAt   *time.Time `json:"created_at`
-	ShippedAt   *time.Time `json:"shipped_at`
-	CompletedAt *time.Time `json:"completed_at`
+	OrderID     uint64     `json:"order_id"`
+	CustomerID  uuid.UUID  `json:"customer_id"`
+	LineItems   []LineItem `json:"line_items"`
+	CreatedAt   *time.Time `json:"created_at"`
+	ShippedAt   *time.Time `json:"shipped_at"`
+	CompletedAt *time.Time `json:"completed_at"`
 }
 
 type LineItem struct {
-	ItemID   uuid.UUID `json:"item_id`
-	Quantity uint      `json:"quantity`
-	Price    uint      `json:"price`
+	ItemID   uuid.UUID `json:"item_id"`
+	Quantity uint      `json:"quantity"`
+	Price    uint      `json:"price"`
 }
